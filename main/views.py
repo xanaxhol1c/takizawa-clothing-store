980377708f66007bc6726f8db37c1eeb5f78b385
xanaxhol1c <loram.hoptan@gmail.com>
2025-02-20 16:51:42 +0200

    created models, migrations and added some changes to views

--- main/views.py
+++ main/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index/index.html')
+
+def get_popular_list(request):
+    products = Product.objects.filter(avaliable=True)[:3]
+    return render(request, 'main/index/index.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, avaliable=True)
+    return render(request, 'main/product/details.html', {'product': product})
