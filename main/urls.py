2698742816e305d0a03931c2aebb68cfc45fae8d
xanaxhol1c <loram.hoptan@gmail.com>
2025-02-24 23:21:01 +0200

    added pagination for products + changes to css

--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('shop/', views.get_products_list, name='shop'),
-    path('shop/category/<slug:category_slug>', views.get_products_list, name='get_products_list'),
     path('shop/<slug:slug>/', views.product_detail,  name='product_detail'),
+    path('shop/category/<slug:category_slug>', views.get_products_list, name='get_products_list'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 def index(request):
     return render(request, 'main/index/index.html')
 
 def get_products_list(request, category_slug = None):
-
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all().order_by('id')
-    products = Product.objects.filter(avaliable=True)
+    products = Product.objects.filter(avaliable=True).order_by('category_id')
 
     if category_slug:
         category = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category=category)
-        products_by_categories = {category: products}
+        paginator = Paginator(products, 8)
+        current_page = paginator.page(int(page))
     else:
-        products_by_categories = {category: Product.objects.filter(avaliable=True, category=category) for category in categories}
+        paginator = Paginator(products, 8)
+        current_page = paginator.page(int(page))
 
-    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products_by_categories': products_by_categories})
+    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'current_page': current_page, 'slug_url' : category_slug})
 
 # def categories_list(request):
 #     categories = Category.objects.all().order_by('id')
-
 
 
 def product_detail(request, slug):
