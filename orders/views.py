3560843b9d74ac2fa7d0d3a8be2ff2e6f2f9fe47
xanaxhol1c <loram.hoptan@gmail.com>
2025-03-14 15:11:48 +0200

    small fixes to order/create.html

--- orders/views.py
+++ orders/views.py
@@ -12,8 +12,11 @@ def order_create(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-        cart.clear()
-        return render(request, 'order/created.html', {'order' : order, 'form' : form})
+            cart.clear()
+            return render(request, 'order/created.html', {'order' : order, 'form' : form})
+        else:
+            return render(request, 'order/create.html', {'cart' : cart, 'form' : form})
+
     else:
         form = OrderCreateForm(request=request)
         return render(request, 'order/create.html', {'cart' : cart, 'form' : form})
