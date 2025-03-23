d59cb2d521b5981f34749eb8807ba30a915c7243
xanaxhol1c <loram.hoptan@gmail.com>
2025-03-23 21:59:42 +0200

    added option to add up to 5 images of product and watch it in product description

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductImage
 # Register your models here.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 5
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -14,3 +19,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['avaliable','created','updated']
     list_editable = ['price', 'avaliable', 'discount']
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
--- main/models.py
+++ main/models.py
@@ -48,7 +48,11 @@ class Product(models.Model):
         
         return self.price
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
 
+    image = models.ImageField(upload_to='produts/%Y/%m/%d', blank=True)
 
-
+    def __str__(self):
+        return f'{self.product.name} - {self.image.name}'
 
