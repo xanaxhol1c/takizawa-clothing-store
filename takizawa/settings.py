fe0f69dfab27868a83217a611306b70b29d46fbc
xanaxhol1c <loram.hoptan@gmail.com>
2025-04-01 18:59:22 +0300

    small fix to settings

--- takizawa/settings.py
+++ takizawa/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_STATUS')
+
+DEBUG = False
+
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(" ")
 
