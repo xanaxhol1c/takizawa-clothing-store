e1b1f05f4cb6dd167192dff180cc9e3e8b8c182b
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-14 18:43:08 +0300

    fixed product-images + collect static

--- main/models.py
+++ main/models.py
@@ -53,7 +53,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
 
-    image = models.ImageField(upload_to='produts/%Y/%m/%d', blank=True)
+    image = CloudinaryField('image')
 
     def __str__(self):
         return f'{self.product.name} - {self.image}'
